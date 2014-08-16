6ca60fb42a21f97b9914be98a5de63a92ebfceba
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-08-15 19:03:17 -0700

    worked on js and some css

--- public/javascripts/index2.js
+++ public/javascripts/index2.js
@@ -1,11 +1,37 @@
 $(function() {
-	//resizeBG();
-	//note
-	//$()
+	// Handle the form submition
+        $('.email-form').submit(function(event) {
+            event.preventDefault();
+            var $form = $(this);
+            if( $form.find('.error-message') ) {
+                clear_error($form);
+            }
+            $.ajax({
+                type: "POST",
+                url: '/process/username',
+                data: $form.serialize(),
+                success: function(data, textStatus, xhr) {
+                    if (data.error) {
+                        $form.find('.error-message').css('display','block').html(data.error).fadeIn();
+                        $form.find('input').addClass('error');
+                    } else {
+                        // TODO: display some sort of success screen??
+                        $('a.thanks').click();
+                    }
+                },
+                error: function() {
+
+                }
+            });
+        });
+
+        // Remove the error class (if any) when the user input changes. Also remove any error messages.
+        $('input').change(function() {
+            clear_error($(this).parent('form'));
+        });
 });
 
-$(window).resize(function() {
-	//resizeBG();
-});
-
-//resize the bg image to the correct size.
+function clear_error(form) {
+	form.find('input').removeClass('error');
+	form.find('.error-message').css('display','none').fadeOut();
+}
